dca51c4f53003fa3b8a3ee8bab9cd141421f57c5
Reggie-Adino <adinoex1greatness@gmail.com>
2023-08-14 19:46:05 +0000

    corrected http to https

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import SearchIcon from './search.svg';
 import axios from 'axios';
 
 
-const API_URL = 'http://www.omdbapi.com/?apikey=975bd47a';
+const API_URL = 'https://www.omdbapi.com/?apikey=975bd47a';
 
 
 const App =() => {
